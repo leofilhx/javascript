30950cd1309f3bca469df556754507911a0ca166
leofilhx <145705812+leofilhx@users.noreply.github.com>
2024-10-31 15:58:10 -0300

    condições js

---
+++ Exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 58
+console.log (`A velocidade do seu carro é ${vel}km por hora`)
+if (vel > 60) {
+    console.log (`Você ultrapassou a velocidade permitida: MULTADO`)
+}
+console.log (`Diriga sempre usando cinto de segurança`)
