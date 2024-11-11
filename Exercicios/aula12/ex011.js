bf5b80ffc9e06d946ed6407581f57b472ccae105
leofilhx <145705812+leofilhx@users.noreply.github.com>
2024-11-11 16:16:57 -0300

    tetstt

---
+++ Exercicios/aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 17
+if (idade < 16) {
+    console.log (`NÂO VOTA`) 
+} else {
+    if (idade >= 16 && idade < 18) {
+        console.log (`VOTO OPCIONAL`)
+    } else {
+        console.log (`VOTA`)
+    }
+}
