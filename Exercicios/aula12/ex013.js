1176850ade7ca6cc3bf80c0e6434015aee20c25c
leofilhx <145705812+leofilhx@users.noreply.github.com>
2024-11-12 15:45:05 -0300

    repetiço~es

---
+++ Exercicios/aula12/ex013.js
@@ -0,0 +1,16 @@
+var agora = new Date()
+var diaSem = 5
+
+switch (diaSem) {
+    case 0:
+        console.log (`Domingo`)
+        break
+    case 1: 
+        console.log (`Segunda-feira`)
+        break
+    case 2:
+        console.log (`Terça-feira`)
+        break
+    default:
+        console.log (`Preguiça`)
+}
